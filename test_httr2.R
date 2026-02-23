ce792f6f6cb233add9fb56ddb8febc9bedca1e16
ivyklo <ivy.ka.lo@gmail.com>
2026-02-22 23:59:02 -0800

    update folder structure

--- test_httr2.R
+++
@@ -1,8 +0,0 @@
-library(httr2)
-
-resp <- request("https://api.github.com/users/octocat") |>
-  req_headers(Accept = "application/json") |>
-  req_perform()
-
-print(resp_status(resp))
-print(resp_body_json(resp))
